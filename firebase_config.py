ff3d604b1d33c57deea8c3c8e6eb836750963f10
Vineet Lakra <vineet416@gmail.com>
2026-01-30 13:34:51 +0530

    Refactor Firebase initialization and add service account credentials

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,25 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import streamlit as st
+import os
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
-    firebase_admin.initialize_app(cred)
+def init_firebase():
+    if not firebase_admin._apps:
 
-db = firestore.client()
+        # 🔹 CASE 1 — Running on Streamlit Cloud
+        if "firebase" in st.secrets:
+            cred = credentials.Certificate(dict(st.secrets["firebase"]))
+
+        # 🔹 CASE 2 — Running locally
+        elif os.path.exists("serviceAccountKey.json"):
+            cred = credentials.Certificate("serviceAccountKey.json")
+
+        else:
+            raise Exception("Firebase credentials not found")
+
+        firebase_admin.initialize_app(cred)
+
+    return firestore.client()
+
+
+db = init_firebase()
